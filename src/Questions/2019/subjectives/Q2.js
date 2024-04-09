960b0abf06dfc812b03d31873a1f2cc5cbd08c7a
abhinavbgpgit <abhinavbgp@gmail.com>
2024-04-09 06:42:30 +0530

    question added

---
+++ src/Questions/2019/subjectives/Q2.js
@@ -0,0 +1,101 @@
+// 501 2019 1a
+// What do you know about HTML tags? How to use to line break, Horizontal line, Anchor, Img and Table tags in HTML? 
+
+// HTML (Hypertext Markup Language) tags are fundamental building blocks used to structure content on the web. They are enclosed in angle brackets < > and are used to define various elements within a web page. Here are some key points about HTML tags:
+
+// Opening and Closing Tags: Most HTML tags come in pairs - an opening tag and a closing tag. The opening tag denotes the beginning of an element, and the closing tag denotes the end. The closing tag is preceded by a forward slash /. For example:
+
+// html
+// Copy code
+// <p>This is a paragraph.</p>
+// Attributes: HTML tags can have attributes that provide additional information about an element. Attributes are specified within the opening tag and are written as name-value pairs. For example:
+
+// html
+// Copy code
+// <a href="https://www.example.com">Click here</a>
+// In this <a> tag, href is an attribute that specifies the URL to which the link points.
+
+// Nesting: HTML elements can be nested inside one another. This means that one element can contain another element. For example:
+
+// html
+// Copy code
+// <div>
+//     <p>This is a paragraph inside a div.</p>
+// </div>
+// Empty Elements: Some HTML elements do not have a closing tag and are called empty elements or self-closing tags. They typically represent content that doesn't need any additional structure. For example:
+
+// html
+// Copy code
+// <img src="image.jpg" alt="Description">
+// In this <img> tag, there is no closing tag because it's self-contained.
+
+// Common HTML Tags: There are many HTML tags, each serving a specific purpose. Some common ones include:
+
+// <p>: Paragraph
+// <a>: Anchor (link)
+// <img>: Image
+// <div>: Division (for layout)
+// <span>: Inline division
+// <h1>, <h2>, ..., <h6>: Headings
+// <ul>: Unordered list
+// <ol>: Ordered list
+// <li>: List item
+// <table>: Table
+// <tr>: Table row
+// <td>: Table cell
+// <form>: Form
+// HTML tags are essential for structuring content on the web and are used in conjunction with CSS (Cascading Style Sheets) and JavaScript to create visually appealing and interactive web pages.
+
+
+
+
+
+
+
+
+
+// Sure, here are examples of how to use each of those HTML tags:
+
+// Line Break <br>:
+// The <br> tag is used to insert a line break in the text. It's a self-closing tag, meaning it doesn't have a closing tag.
+
+// html
+// Copy code
+// This is some text.<br>
+// This text will appear on a new line.
+// Horizontal Line <hr>:
+// The <hr> tag is used to insert a horizontal line across the width of its containing element. It's also a self-closing tag.
+
+// html
+// Copy code
+// <p>This is some text above the line.</p>
+// <hr>
+// <p>This is some text below the line.</p>
+// Anchor <a>:
+// The <a> tag is used to create hyperlinks. It requires an href attribute that specifies the URL of the linked page.
+
+// html
+// Copy code
+// <a href="https://www.example.com">Visit Example.com</a>
+// Image <img>:
+// The <img> tag is used to embed images in an HTML document. It requires the src attribute, which specifies the URL of the image, and the alt attribute, which provides alternative text for accessibility.
+
+// html
+// Copy code
+// <img src="image.jpg" alt="Description of the image">
+// Table <table>:
+// The <table> tag is used to create tables in HTML. Tables are constructed with a combination of <table>, <tr> (table row), <td> (table data/cell), and optionally <th> (table header cell) tags.
+
+// html
+// Copy code
+// <table border="1">
+//     <tr>
+//         <th>Heading 1</th>
+//         <th>Heading 2</th>
+//     </tr>
+//     <tr>
+//         <td>Data 1</td>
+//         <td>Data 2</td>
+//     </tr>
+// </table>
+// These are the basic uses of the mentioned HTML tags. They can be further styled and customized using CSS to enhance the appearance and functionality of web pages.
